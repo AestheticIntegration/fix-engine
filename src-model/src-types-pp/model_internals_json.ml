ddef03a6341456f5af776e2f6a869d21c3c6fcc1
Ewen Maclean <ewenmaclean@gmail.com>
2018-10-18 09:55:21 +0100

    wip: updating to iml

--- src-model/src-types-pp/model_internals_json.ml
+++
@@ -1,6 +0,0 @@
-(* Aesthetic Integration copyright 2018 *)
-open State;;
-
-let json_of_fix_internals (_x : internal_states_types)  =
-    `Assoc []
-;;
--- src-model/src/msg_check_validate.ml
+++
@@ -1,8 +0,0 @@
-(* Aesthetic Integration copyright 2018 *)
-open Model_messages;;
-open Msg_check_types;;
-open State;;
-
-let check_validate_NewOrderSingle (_msg_data : mod_newordersingle_data) (_m_state : model_state)  =
-    no_invalid_validates
-;;
